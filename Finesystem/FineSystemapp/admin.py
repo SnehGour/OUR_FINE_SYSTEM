f6fadd655b1691642fada80371f371515b049213
unknown <dubeybulbul111@gmail.com>
2020-03-16 02:22:40 -0700

    add Fine_apply

--- Finesystem/FineSystemapp/admin.py
+++ Finesystem/FineSystemapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import login
+from .models import login,Fine_apply
 # Register your models here.
 admin.site.register(login)
+admin.site.register(Fine_apply)
--- Finesystem/FineSystemapp/models.py
+++ Finesystem/FineSystemapp/models.py
@@ -2,8 +2,18 @@ from django.db import models
 # Create your models here.
 
 class login(models.Model):
-    Username=models.CharField(max_length=100)
+    Username=models.CharField(max_length=100 ,primary_key=True)
     Password=models.CharField(max_length=100)
 
     def __str__(self):
         return self.Username
+class Fine_apply(models.Model):
+    Username = models.ForeignKey(login,on_delete=models.CASCADE)
+    Rule =models.CharField(max_length=265)
+    Fine_Amount =models.IntegerField()
+    Date=models.DateTimeField()
+
+    def __str__(self):
+        a=self.Username
+        b=str(self.Fine_Amount)
+        return a+b
